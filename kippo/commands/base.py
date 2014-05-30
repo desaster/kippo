4730c490b01e923cc59684df40896fc7b2e8e8d7
g0tmi1k <have.you.g0tmi1k@gmail.com>
2014-05-30 04:19:37 +0100

    Add "bash -c" response Credit: https://github.com/micheloosterhof/kippo

--- kippo/commands/base.py
+++ kippo/commands/base.py
@@ -291,6 +291,15 @@ class command_yes(HoneyPotCommand):
         self.exit()
 commands['/usr/bin/yes'] = command_yes
 
+class command_sh(HoneyPotCommand):
+    def call(self):
+        if len(self.args) and self.args[0].strip() == '-c':
+            self.honeypot.cmdstack[0].cmdpending.append( ' '.join( self.args[1:] ) )
+        else:
+            pass
+commands['/bin/bash'] = command_sh
+commands['/bin/sh'] = command_sh
+
 class command_nop(HoneyPotCommand):
     def call(self):
         pass
@@ -298,8 +307,6 @@ commands['/bin/chmod'] = command_nop
 commands['set'] = command_nop
 commands['unset'] = command_nop
 commands['export'] = command_nop
-commands['/bin/bash'] = command_nop
-commands['/bin/sh'] = command_nop
 commands['/bin/kill'] = command_nop
 commands['/bin/su'] = command_nop
 
