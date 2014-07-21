8c0b92140f243a53384805f3e6f5edbaae008d02
mk-pmb <mk-pmb@users.noreply.github.com>
2014-07-21 22:46:34 +0200

    start/stop: find symlinked $0; stop: chmod+x, integer PIDs only

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # -*- coding: utf-8, tab-width: 2 -*-
-cd "$(dirname "$0")" || exit $?
+cd "$(readlink -m "$0"/..)" || exit $?
 tty --silent && echo 'Starting kippo in the background...'
 twistd -y kippo.tac -l log/kippo.log --pidfile kippo.pid || exit $?
