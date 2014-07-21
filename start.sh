50937f2ed67bb03a643744b1dae4d3c3104ada38
mk-pmb <mk-pmb@users.noreply.github.com>
2014-07-21 22:33:02 +0200

    start.sh: report twistd's return value if error

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 # -*- coding: utf-8, tab-width: 2 -*-
 cd "$(dirname "$0")" || exit $?
 tty --silent && echo 'Starting kippo in the background...'
-twistd -y kippo.tac -l log/kippo.log --pidfile kippo.pid
+twistd -y kippo.tac -l log/kippo.log --pidfile kippo.pid || exit $?
