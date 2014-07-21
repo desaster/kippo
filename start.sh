3e7a2c79328da3236d558fc87ec4103ad29bf9f9
mk-pmb <mk-pmb@users.noreply.github.com>
2014-07-21 22:31:06 +0200

    start.sh: fail when cd fails, silent on non-tty

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
-#!/bin/sh
-
-echo "Starting kippo in the background...\n"
-cd $(dirname $0)
+#!/bin/bash
+# -*- coding: utf-8, tab-width: 2 -*-
+cd "$(dirname "$0")" || exit $?
+tty --silent && echo 'Starting kippo in the background...'
 twistd -y kippo.tac -l log/kippo.log --pidfile kippo.pid
-
