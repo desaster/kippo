b79305412d6bedc3a3505165e107161bdaca82f0
Finn <thefinn93@thefinn93.com>
2014-11-22 18:53:56 -0800

    Added an IRC logger

---
+++ kippo/dblog/irc.py
@@ -0,0 +1,85 @@
+from kippo.core import dblog
+from twisted.internet import defer
+from twisted.python import log
+from twisted.python import log
+from asyncirc.ircbot import IRCBot
+import threading
+
+class DBLogger(dblog.DBLogger):
+    def start(self, cfg):
+        if cfg.has_option('database_irc', 'port'):
+            port = int(cfg.get('database_irc', 'port'))
+        else:
+            port = 6667
+
+        nick = self.getSensor()
+        if cfg.has_option('database_irc', 'nick'):
+            nick = cfg.get('database_irc', 'nick')
+        if nick is None:
+            import random
+            import string
+            nick = ''.join(random.choice(string.ascii_lowercase) for _ in range(8))
+
+        self.channels = ['kippo-events']
+        if cfg.has_option('database_irc', 'channel'):
+            self.channels = cfg.get('database_irc', 'channel').split(",")
+
+        server = 'irc.efnet.org'
+        if cfg.has_option('database_irc', 'server'):
+            server = cfg.get('database_irc', 'server')
+
+        password = None
+        if cfg.has_option('database_irc', 'password'):
+            password = cfg.get('database_irc', 'password')
+
+        ssl =  False
+        if cfg.has_option('database_irc', 'ssl'):
+            ssl = cfg.get('database_irc', 'ssl') in ('1', 'true', 'yes')
+
+        self.connection = IRCBot(server, port, nick, 'Kippo', password, ssl)
+        self.connection.start()
+        for channel in self.channels:
+            self.connection.join(channel)
+
+    def write(self, session, message):
+        if self.connection:
+            for channel in self.channels:
+                self.connection.msg(channel, "[%s] %s" % (session, message))
+
+    def createSession(self, peerIP, peerPort, hostIP, hostPort):
+        sid = uuid.uuid1().hex
+        sensorname = self.getSensor() or hostIP
+        self.write(sid, 'New connection: %s:%s' % (peerIP, peerPort))
+        return sid
+
+    def handleConnectionLost(self, session, args):
+        self.write(session, 'Connection lost')
+
+    def handleLoginFailed(self, session, args):
+        self.write(session, 'Login failed [%s/%s]' % \
+            (args['username'], args['password']))
+
+    def handleLoginSucceeded(self, session, args):
+        self.write(session, 'Login succeeded [%s/%s]' % \
+            (args['username'], args['password']))
+
+    def handleCommand(self, session, args):
+        self.write(session, 'Command [%s]' % (args['input'],))
+
+    def handleUnknownCommand(self, session, args):
+        self.write(session, 'Unknown command [%s]' % (args['input'],))
+
+    def handleInput(self, session, args):
+        self.write(session, 'Input [%s] @%s' % (args['input'], args['realm']))
+
+    def handleTerminalSize(self, session, args):
+        self.write(session, 'Terminal size: %sx%s' % \
+            (args['width'], args['height']))
+
+    def handleClientVersion(self, session, args):
+        self.write(session, 'Client version: [%s]' % (args['version'],))
+
+    def handleFileDownload(self, session, args):
+        self.write(session, 'File download: [%s] -> %s' % \
+            (args['url'], args['outfile']))
+# vim: set sw=4 et:
